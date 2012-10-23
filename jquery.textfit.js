798262e33f0d93eda86f2b5bb781de11d8e0a6e6
Everton Fraga <everton.frag@gmail.com>
2012-10-23 15:54:44 -0200

    Improvements on decreasing font-size stop condition; Adding experimental ellipsis feature

--- jquery.textfit.js
+++ jquery.textfit.js
@@ -1,28 +1,68 @@
 /*
 TextFit, a simple plugin that decreases the font-size to fit an element.
-@author: Everton Fraga <everton.fraga@corp.globo.com>
+@author: Everton Fraga http://github.com/evertonfraga/TextFit
 */
+    
 (function($){
-$.fn.textFit = function(options) {
-	var defaults = { minFontSize: 28 };
-	options = $.extend(defaults, options);
-
-	$(this).each(function(i, el){
-		var $el = $(el);
-		var oldFontSize = $el.data('oldfontsize');
-		if (oldFontSize) {
-			$el.css('font-size', oldFontSize + 'px');
-		}
-		
-		var lineHeight = parseInt($el.css('line-height'), 10),
-		fontSize = parseInt($el.css('font-size'), 10),
-		height = parseInt($el.height(), 10);
-		$el.data('oldfontsize', fontSize);
-		
-		while ((height > lineHeight) && (fontSize > options.minFontSize)) {
-			$el.css('font-size', --fontSize);
-			height = parseInt($el.height(), 10);
-		}
-	});
-};
+    
+    $.fn.textFit = function(options) {
+        
+        var defaults = { lines: 1, minFontSize: 28, action: 'shrink', clearHeight: false};
+        options = $.extend(defaults, options);
+    
+        var shrinkText = function(el) {
+            var $el = $(el),
+                checkHeight = function() {
+                    lineHeight = parseInt($el.css('line-height'), 10);
+                    height = parseInt($el.height(), 10);
+                    // if (!lineHeight && console) console.warn('Elemento sem line-height.', $el);
+                    // if (!height && console) console.warn('Elemento sem height, com display inline ou oculto.', $el);
+                    return height > lineHeight * options.lines;
+                },
+                checkFontLimit = function() {
+                    fontSize = parseInt($el.css('font-size'), 10);
+                    return fontSize > options.minFontSize;
+                };
+    
+            while (checkHeight() && checkFontLimit()) {
+                $el.css('font-size', --fontSize);
+                height = parseInt($el.height(), 10);
+            }
+        };
+    
+        var ellipsis = function(el) {
+            var $el = $(el);
+            var text = $el.html();
+            var hiddenNode = $(el.cloneNode(true)).css({
+                'display': 'none',
+                'position': 'absolute',
+                'overflow': 'visible',
+                'height': 'auto',
+                'width': $el.width
+            });
+    
+            $el.after(hiddenNode);
+    
+            while (text.length > 0 && (hiddenNode.height() > $el.height())) {
+                text = text.substr(0, text.length - 1);
+                hiddenNode.html(text + "...");
+            }
+    
+            if (($el.html() == hiddenNode.html()) && (options.clearHeight)) {
+                $el.css('height','auto');
+            }
+    
+            $el.html(hiddenNode.html());
+            hiddenNode.remove();
+        };
+    
+        $(this).each(function(i, el){
+            if (options.action=='ellipsis') {
+                ellipsis(el);
+            } else {
+                shrinkText(el);
+            }
+        });
+    };
+    
 })(jQuery);
